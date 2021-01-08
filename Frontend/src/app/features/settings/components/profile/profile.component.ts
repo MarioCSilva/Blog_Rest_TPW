3d6de8cf958ae62037c92827391ac9cc7590fc61
BrunosBastos <Brunosb@ua.pt>
2021-01-08 15:29:46 +0000

    edit profile almost working

--- Frontend/src/app/features/settings/components/profile/profile.component.ts
+++ Frontend/src/app/features/settings/components/profile/profile.component.ts
@@ -11,20 +11,29 @@ export class ProfileComponent implements OnInit {
   client: Client;
   owner: boolean;
   updating: boolean;
+  updateClient: Client;
   constructor(private profileService: ProfileService) { }
 
   ngOnInit(): void {
     this.updating = false;
     this.profileService.getProfile('olasounovoaqui40').subscribe(data => {
-      console.log(data);
       this.client = data['client'];
       this.owner = data['owner'];
       this.client.profile_pic = this.client.profile_pic == null ? '' : this.client.profile_pic  ;
+      this.updateClient = this.client;
     });
   }
 
   updateProfile(): void {
-    console.log('updating profile');
+    // TODO: change the error message
+    // TODO: change date format to work with the rest
+    this.profileService.updateProfile(this.updateClient).subscribe(data => {console.log('working'); this.client = this.updateClient; },
+        error => {console.log(error); });
+  }
+
+  discardChanges(): void{
+    this.updateClient = this.client;
+    this.ngOnInit();
   }
 
 }
